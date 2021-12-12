2545a00d0ee140b1af041333c32a3394e99f6bae
Ayumi Ito <56537575+cha-ayu@users.noreply.github.com>
2021-12-12 23:07:24 +0900

    edit Layout etc.

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,10 +1,11 @@
 import Link from 'next/link'
 import Head from 'next/head'
+import Layout from '../../components/Layout'
 
 
 export default function FirstPost() {
   return (
-    <>
+    <Layout>
       <Head>
         <title>First Post</title>
       </Head>
@@ -14,8 +15,7 @@ export default function FirstPost() {
           <a>Back to home</a>
         </Link>
       </h2>
-
-    </>
+    </Layout>
     )
 }
 
